6aa637524923e91ba896b7fdfbea497b69fcd7fb
Tamara <tamaramoraisfrigo@gmail.com>
2025-07-05 09:25:18 -0400

    fix(frontend): prevent duplicate API calls on DuvList by guarding useEffect with useRef

--- front-duv/src/main.jsx
+++ front-duv/src/main.jsx
@@ -1,17 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import DuvDetails from "./pages/DuvDetails";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/duv/:id" element={<DuvDetails />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>
 );
